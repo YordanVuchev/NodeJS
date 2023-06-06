d7f32e9929c3bdc45b042ee83a4ce70b5d37f935
YordanVuchev <danivu4e@abv.bg>
2023-06-06 10:32:02 +0300

    Learning ejs

--- Express/ExpressJS/app.js
+++ Express/ExpressJS/app.js
@@ -1,19 +1,10 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const expressHbs = require('express-handlebars')
 
 const app = express()
 
-app.engine(
-  'hbs',
-  expressHbs({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-  })
-)
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 const adminData = require('./routes/admin')
@@ -26,7 +17,9 @@ app.use('/admin', adminData.routes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-  res.status(404).render('page-not-found', { pageTitle: 'Page not found' })
+  res
+    .status(404)
+    .render('page-not-found', { pageTitle: 'Page not found', path: undefined })
 })
 
 app.listen(3000)
