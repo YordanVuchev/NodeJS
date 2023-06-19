36a7d5469f3581e7af3d216b6ac7f39e562120f9
YordanVuchev <danivu4e@abv.bg>
2023-06-19 10:25:58 +0300

    Dynamic params

--- Dynamic routes/controllers/shop.js
+++ Dynamic routes/controllers/shop.js
@@ -1,42 +1,59 @@
-const Product = require('../models/product');
+const Product = require('../models/product')
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
-      path: '/products'
-    });
-  });
-};
+      path: '/products',
+    })
+  })
+}
+
+exports.getProduct = (req, res, next) => {
+  const productId = req.params.productId
+  Product.findById(productId, (product) => {
+    res.render('shop/product-detail', {
+      product,
+      pageTitle: product.title,
+      path: '/products',
+    })
+  })
+}
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll((products) => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
-      path: '/'
-    });
-  });
-};
+      path: '/',
+    })
+  })
+}
 
 exports.getCart = (req, res, next) => {
   res.render('shop/cart', {
     path: '/cart',
-    pageTitle: 'Your Cart'
-  });
-};
+    pageTitle: 'Your Cart',
+  })
+}
+
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId
+  console.log(productId)
+  res.redirect('/')
+}
 
 exports.getOrders = (req, res, next) => {
   res.render('shop/orders', {
     path: '/orders',
-    pageTitle: 'Your Orders'
-  });
-};
+    pageTitle: 'Your Orders',
+  })
+}
 
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
-    pageTitle: 'Checkout'
-  });
-};
+    pageTitle: 'Checkout',
+  })
+}
