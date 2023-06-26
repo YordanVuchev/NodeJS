adf90f5163ba95428763225457bc88a93ce7a71f
YordanVuchev <danivu4e@abv.bg>
2023-06-26 09:53:40 +0300

    Working with Sequelize

--- Dynamic routes/controllers/shop.js
+++ Dynamic routes/controllers/shop.js
@@ -2,25 +2,32 @@ const Product = require('../models/product')
 const Cart = require('../models/cart')
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render('shop/product-list', {
-        prods: rows,
+        prods: products,
         pageTitle: 'All Products',
         path: '/products',
       })
     })
-    .catch((err) => {
-      console.log(err)
-    })
+    .catch((err) => console.log(err))
 }
 
 exports.getProduct = (req, res, next) => {
   const productId = req.params.productId
-  Product.findById(productId)
-    .then(([product]) => {
+  // Product.findAll({ where: { id: productId } })
+  //   .then((products) =>
+  //     res.render('shop/product-detail', {
+  //       product: products[0],
+  //       pageTitle: products[0].title,
+  //       path: '/products',
+  //     })
+  //   )
+  //   .catch((err) => console.log(err))
+  Product.findByPk(productId)
+    .then((product) => {
       res.render('shop/product-detail', {
-        product: product[0],
+        product: product,
         pageTitle: product.title,
         path: '/products',
       })
@@ -29,10 +36,10 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-    .then(([rows, fieldData]) => {
+  Product.findAll()
+    .then((products) => {
       res.render('shop/index', {
-        prods: rows,
+        prods: products,
         pageTitle: 'Shop',
         path: '/',
       })
