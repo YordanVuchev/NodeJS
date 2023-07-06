3724c0cc063c26cba857637a8ea75742633a69b6
YordanVuchev <danivu4e@abv.bg>
2023-07-06 15:06:18 +0300

    mongo db init

--- MongoDB/util/database.js
+++ MongoDB/util/database.js
@@ -1,8 +1,18 @@
-const Sequelize = require('sequelize')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-const sequelize = new Sequelize('node-complete', 'root', '12345', {
-  dialect: 'mysql',
-  host: 'localhost',
-})
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    'mongodb+srv://yordanyvuchev:12345@cluster0.ymewh8y.mongodb.net/'
+  )
+    .then((client) => {
+      console.log('Connected')
+      callback(client)
+    })
+    .catch((err) => {
+      console.log('Connection failed')
+      console.log(err)
+    })
+}
 
-module.exports = sequelize
+module.exports = mongoConnect
