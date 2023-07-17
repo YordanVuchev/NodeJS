9f8b44ba2d400c78e40d4a9a646724c658cee601
YordanVuchev <danivu4e@abv.bg>
2023-07-17 22:28:21 +0300

    Quick refresher of the basics

---
+++ Course_2/1-node-farm/starter/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const http = require('http')
+
+///////////////////////
+// FILES
+
+//Blocking sync way
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+const textOut = `This is what we know about avocado: ${textIn}.\nCreated on: ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt', textOut)
+
+//Non-blocking async way
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2)
+//   })
+// })
+
+///////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server')
+})
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000')
+})
