7723c16ee6e45ab01237406e1f27576a542fb346
YordanVuchev <danivu4e@abv.bg>
2023-07-17 22:58:31 +0300

    Building simple api

--- Course_2/1-node-farm/starter/index.js
+++ Course_2/1-node-farm/starter/index.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
 const http = require('http')
+const url = require('url')
 
 ///////////////////////
 // FILES
 
 //Blocking sync way
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-const textOut = `This is what we know about avocado: ${textIn}.\nCreated on: ${Date.now()}`
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+// const textOut = `This is what we know about avocado: ${textIn}.\nCreated on: ${Date.now()}`
 // fs.writeFileSync('./txt/output.txt', textOut)
 
 //Non-blocking async way
@@ -19,8 +20,28 @@ const textOut = `This is what we know about avocado: ${textIn}.\nCreated on: ${D
 ///////////////////////
 // SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data)
+
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server')
+  const pathName = req.url
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the overview')
+  } else if (pathName === '/product') {
+    res.end('This is the product')
+  } else if (pathName === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    })
+    res.end(data)
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    })
+    res.end('<h1>Page not found!</h1>')
+  }
 })
 
 server.listen(8000, '127.0.0.1', () => {
