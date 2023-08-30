00a4329aa7b597caf7fe3f34ba1f3afef97af40c
YordanVuchev <danivu4e@abv.bg>
2023-08-30 17:58:24 +0300

    Error handling

--- Course_2/natours/app.js
+++ Course_2/natours/app.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const morgan = require('morgan')
 
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
+const tourRouter = require('./routes/tourRoutes')
+const userRouter = require('./routes/userRoutes')
 
-const app = express();
+const app = express()
 // Middlewares
 if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+  app.use(morgan('dev'))
 }
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+  req.requestTime = new Date().toISOString()
+  next()
+})
 
 //Routes
 
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use('/api/v1/tours', tourRouter)
+app.use('/api/v1/users', userRouter)
 
-//START SERVER
-module.exports = app;
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on the server!`, 404))
+})
+
+app.use(globalErrorHandler)
+
+module.exports = app
